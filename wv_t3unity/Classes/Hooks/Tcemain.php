a0b03eea1af3f983d7eecba4ef572e4ad5d7fdb2
Eydamos <t.werdin@eydamos.de>
2015-04-16 10:34:57 +0200

    [BUGFIX] Set correct column ids and reworked unity_path generation
    * Column id 0 was resulting in not adding the param to the widget in
      magento
    * Path generation was not working for new sites

--- wv_t3unity/Classes/Hooks/Tcemain.php
+++ wv_t3unity/Classes/Hooks/Tcemain.php
@@ -1,6 +1,9 @@
 <?php
 namespace WebVision\WvT3unity\Hooks;
 
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Backend\Utility\BackendUtility;
+
 /***************************************************************
  *
  *  Copyright notice
@@ -26,9 +29,8 @@ namespace WebVision\WvT3unity\Hooks;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-
-class Tcemain {
-
+class Tcemain
+{
     /**
      * A TCEmain hook to expire old records and add new ones
      *
@@ -38,161 +40,85 @@ class Tcemain {
      * @param array $databaseData
      * @return void
      */
-    public function processDatamap_afterDatabaseOperations($status, $tableName, $recordId, array $databaseData) {
-
-        if ($tableName == 'pages') {
-
-	        $recordPath = self::getRecordPath($recordId,'',1000);
-	        $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid =' . $recordId, array('unity_path' => $recordPath) );
-
-	        $treeRecords = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($recordId);
-
-            foreach ($treeRecords as $row) {
-				if ($row['uid'] > 0) {
-  	                $recordPath = self::getRecordPath($row['uid'],'',1000);
-                    $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid =' . $row['uid'], array('unity_path' => $this->buildPath($recordPath)) );
-				}
-            }
-
-	        $childRecords = self::getPageChilds($recordId);
-	        foreach ($childRecords as $row) {
-		        if ($row['uid'] > 0) {
-			        $recordPath = self::getRecordPath($row['uid'],'',1000);
-			        $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid =' . $row['uid'], array('unity_path' => $this->buildPath($recordPath)) );
-		        }
-	        }
-
+    public function processDatamap_afterDatabaseOperations($status, $tableName, $recordId, array $databaseData, $dataHandler)
+    {
+        // new entry via drag & drop don't process
+        if ($recordId === 'NEW12345') {
+            return;
         }
-        if ($tableName == 'pages_language_overlay') {
-	        $titleLimit = 1000;
-	        $output = ($fullOutput = '/');
 
-	        $record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord('pages_language_overlay',$recordId);
-	        /** @var \TYPO3\CMS\Frontend\Page\PageRepository $pageRepository */
-	        $pageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
-	        $pageRepository->sys_language_uid = $record['sys_language_uid'];
-
-	        $rootline = new \TYPO3\CMS\Core\Utility\RootlineUtility($record['pid'],'',$pageRepository);
-	        foreach($rootline->get() as $row) {
-		        if ($row['uid'] === 0) {
-			        continue;
-		        }
-		        if (array_key_exists('is_siteroot',$row)) {
-			        if ($row['is_siteroot'] == 0) {
-				        print_r($row);
-
-				        if ($row['nav_title'] !== '') {
-					        $output = '/' . strip_tags($row['nav_title']) . $output;
-				        } else {
-					        $output = '/' . strip_tags($row['title']) . $output;
-				        }
-			        }
-		        }
-	        }
-	        $recordPath = self::buildPath($output);
-
-	        $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages_language_overlay', 'uid =' . $recordId, array('unity_path' => $this->buildPath($recordPath)) );
+        // new normal entry has hashed record id, get real id from data handler
+        if ($status == 'new' && strpos($recordId, 'NEW') === 0) {
+            /** @var \TYPO3\CMS\Core\DataHandling\DataHandler $dataHandler */
+            $recordId = $dataHandler->substNEWwithIDs[$recordId];
         }
+
+        switch ($tableName) {
+            case 'pages':
+                $pagesUid = $recordId;
+                $sysLanguageUid = 0;
+                break;
+            case 'pages_language_overlay':
+                $pagesUid = $dataHandler->checkValue_currentRecord['pid'];
+                $sysLanguageUid = $dataHandler->checkValue_currentRecord['sys_language_uid'];
+                break;
+            default:
+                return;
+        }
+
+        /** @var \TYPO3\CMS\Core\Database\DatabaseConnection $db */
+        $db = $GLOBALS['TYPO3_DB'];
+
+        $unityPath = $this->getRecordPath($pagesUid, $sysLanguageUid);
+        $db->exec_UPDATEquery($tableName, 'uid =' . $recordId, array('unity_path' => $unityPath));
+
+        /*
+        // WVTODO this goes up the tree but we need code down the tree
+        $treeRecords = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($recordId);
+
+        foreach ($treeRecords as $row) {
+
+            $recordPath = self::getRecordPath($row['uid'],'',1000);
+            $db->exec_UPDATEquery('pages_language_overlay', 'uid =' . $row['uid'], array('unity_path' => $this->buildPath($recordPath)) );
+        }
+        */
     }
 
-	/**
-	 * buildPath
-	 *
-	 * @param $recordPath
-	 *
-	 * @return string
-	 */
-	protected function buildPath($recordPath) {
-		$pathLong = str_replace(' ','-',strtolower($recordPath));
-		$path = substr($pathLong, 0, -1);
-		if ($path ==  '') {
-			$path = 'index';
-		}
-		return $path . '.html';
-	}
+    /**
+     * getRecordPath
+     *
+     * @param int $uid
+     * @param int $sysLanguageUid
+     *
+     * @return array|string
+     */
+    protected function getRecordPath($uid, $sysLanguageUid)
+    {
+        $output = '';
+        /** @var \TYPO3\CMS\Frontend\Page\PageRepository $pageRepo */
+        $pageRepo = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+        $pageRepo->sys_language_uid = $sysLanguageUid;
+        $data = $pageRepo->getRootLine($uid);
+        ksort($data);
+        foreach ($data as $record) {
+            if ($record['is_siteroot'] == '1') {
+                continue;
+            }
 
-	/**
-	 * getRecordPath
-	 *
-	 * @param     $uid
-	 * @param     $clause
-	 * @param     $titleLimit
-	 * @param int $fullTitleLimit
-	 *
-	 * @return array|string
-	 */
-	static public function getRecordPath($uid, $clause, $titleLimit, $fullTitleLimit = 0) {
-		if (!$titleLimit) {
-			$titleLimit = 1000;
-		}
-		$loopCheck = 100;
-		$output = ($fullOutput = '/');
-		$clause = trim($clause);
-		if ($clause !== '' && substr($clause, 0, 3) !== 'AND') {
-			$clause = 'AND ' . $clause;
-		}
-		$data = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($uid, $clause);
-		foreach ($data as $record) {
-			if ($record['uid'] === 0) {
-				continue;
-			}
-			if (array_key_exists('is_siteroot',$record)) {
-				if ($record['is_siteroot'] == 0) {
-					if ($record['nav_title'] !== '') {
-						$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['nav_title']), $titleLimit) . $output;
-					} else {
-						$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['title']), $titleLimit) . $output;
-					}
-					if ($fullTitleLimit) {
-						if ($record['nav_title'] !== '') {
-							$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['nav_title']), $titleLimit) . $fullOutput;
-						} else {
-							$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['title']), $titleLimit) . $fullOutput;
-						}
-					}
-				}
-			} else {
-				if ($record['nav_title'] !== '') {
-					$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['nav_title']), $titleLimit) . $output;
-				} else {
-					$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['title']), $titleLimit) . $output;
-				}
-				if ($fullTitleLimit) {
-					if ($record['nav_title'] !== '') {
-						$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['nav_title']), $titleLimit) . $fullOutput;
-					} else {
-						$output = '/' . \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs(strip_tags($record['title']), $titleLimit) . $fullOutput;
-					}
-				}
-			}
-		}
-		if ($fullTitleLimit) {
-			return array($output, $fullOutput);
-		} else {
-			return $output;
-		}
-	}
+            $output .= '/' . $this->cleanTitle($record['nav_title'] ? $record['nav_title'] : $record['title']);
+        }
 
-	/**
-	 * getPageChilds
-	 *
-	 * @param        $uid
-	 * @param string $clause
-	 *
-	 * @return array
-	 */
-	static protected function getPageChilds($uid, $clause = '') {
-		static $getPageChilds_cache = array();
-		$ident = $uid . '-' . $clause;
-		if (is_array($getPageChilds_cache[$ident])) {
-			$row = $getPageChilds_cache[$ident];
-		} else {
-			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('pid,uid,title,TSconfig,is_siteroot,storage_pid,t3ver_oid,t3ver_wsid,t3ver_state,t3ver_stage,backend_layout_next_level', 'pages', 'pid=' . (int)$uid . ' ' . \TYPO3\CMS\Backend\Utility\BackendUtility::deleteClause('pages') . ' ' . $clause);
-			while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-				$result[] = $row;
-			}
-			$GLOBALS['TYPO3_DB']->sql_free_result($res);
-		}
-		return $result;
-	}
+        $output = str_replace(' ', '-', strtolower($output));
+
+        if(strlen($output) == 0) {
+            $output = '/index';
+        }
+
+        return $output . '.html';
+    }
+
+    protected function cleanTitle($title)
+    {
+        return urlencode(strip_tags($title));
+    }
 }
