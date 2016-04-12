2984b0493d0108db36db1a5ce93f709b0384d100
Eydamos <t.werdin@eydamos.de>
2016-04-12 11:25:33 +0200

    [TASK] Fixed several request from PR

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -15,55 +15,62 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$locallangDb = 'LLL:EXT:wv_t3unity/Resources/Private/Language/locallang_db.xlf:tt_content.';
+call_user_func(
+    function () {
+        $locallangDb = 'LLL:EXT:wv_t3unity/Resources/Private/Language/locallang_db.xlf:tt_content.';
 
-$additionalColumns = array(
-    'hidden_xs' => array(
-        'exclude' => 1,
-        'label'   => $locallangDb . 'hidden_xs',
-        'config'  => array(
-            'type' => 'check',
-        ),
-    ),
-    'hidden_sm' => array(
-        'exclude' => 1,
-        'label'   => $locallangDb . 'hidden_sm',
-        'config'  => array(
-            'type' => 'check',
-        ),
-    ),
-    'hidden_md' => array(
-        'exclude' => 1,
-        'label'   => $locallangDb . 'hidden_md',
-        'config'  => array(
-            'type' => 'check',
-        ),
-    ),
-    'hidden_lg' => array(
-        'exclude' => 1,
-        'label'   => $locallangDb . 'hidden_lg',
-        'config'  => array(
-            'type' => 'check',
-        ),
-    ),
+        $additionalColumns = array(
+            'hidden_xs' => array(
+                'exclude' => 1,
+                'label'   => $locallangDb . 'hidden_xs',
+                'config'  => array(
+                    'type' => 'check',
+                ),
+            ),
+            'hidden_sm' => array(
+                'exclude' => 1,
+                'label'   => $locallangDb . 'hidden_sm',
+                'config'  => array(
+                    'type' => 'check',
+                ),
+            ),
+            'hidden_md' => array(
+                'exclude' => 1,
+                'label'   => $locallangDb . 'hidden_md',
+                'config'  => array(
+                    'type' => 'check',
+                ),
+            ),
+            'hidden_lg' => array(
+                'exclude' => 1,
+                'label'   => $locallangDb . 'hidden_lg',
+                'config'  => array(
+                    'type' => 'check',
+                ),
+            ),
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+            'tt_content',
+            'responsive',
+            'hidden_xs;'
+            . $locallangDb
+            . 'hidden_xs, hidden_sm;'
+            . $locallangDb
+            . 'hidden_sm, hidden_md;'
+            . $locallangDb
+            . 'hidden_md, hidden_lg;'
+            . $locallangDb
+            . 'hidden_lg'
+        );
+
+        // WVTODO add palett responsive to all types after frames
+        /*\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            '--palette--;Responsive;responsive',
+            '',
+            'after:frames'
+        );*/
+    }
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
-    'tt_content',
-    'responsive',
-    'hidden_xs;'
-    . $locallangDb
-    . 'hidden_xs, hidden_sm;'
-    . $locallangDb
-    . 'hidden_sm, hidden_md;'
-    . $locallangDb
-    . 'hidden_md, hidden_lg;'
-    . $locallangDb
-    . 'hidden_lg'
-);
-// WVTODO add palett responsive to all types after frames
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '--palette--;Responsive;responsive', '', 'after:frames');
-
-unset($additionalColumns);
-unset($locallangDb);
