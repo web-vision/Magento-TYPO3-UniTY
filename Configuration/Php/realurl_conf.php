c2961e8048683c34291edb52a6d780c877d7f0ed
Eydamos <t.werdin@eydamos.de>
2016-04-27 11:25:13 +0200

    [TASK] Updated realurl_conf

--- Configuration/Php/realurl_conf.php
+++ Configuration/Php/realurl_conf.php
@@ -14,18 +14,10 @@
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
     '_DEFAULT' => array(
         'init'        => array(
-            'enableCHashCache'     => true,
-            'appendMissingSlash'   => 'ifNotFile,redirect',
-            'adminJumpToBackend'   => true,
-            'enableUrlDecodeCache' => true,
-            'enableUrlEncodeCache' => true,
-            'emptyUrlReturnValue'  => '/',
+            'appendMissingSlash'  => 'ifNotFile,redirect',
+            'emptyUrlReturnValue' => '/cms/',
         ),
         'pagePath'    => array(
-            'type'           => 'user',
-            'userFunc'       => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-            'spaceCharacter' => '-',
-            'languageGetVar' => 'L',
             'rootpage_id'    => '1',
         ),
         'fileName'    => array(
