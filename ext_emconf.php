35371f9bfa098ac6847940cf24d59196dde9f8f6
Justus Moroni <j.moroni@web-vision.de>
2017-07-20 11:12:14 +0200

    [TASK] Increase version constraint
    * Extension is cms 8 compatible

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '3.0.1',
     'constraints'      => array(
         'depends'   => array(
-            'typo3'   => '7.0.0-7.99.99',
+            'typo3'   => '7.0.0-8.99.99',
             'realurl' => '2.0.0-2.99.99',
         ),
         'conflicts' => array(),
