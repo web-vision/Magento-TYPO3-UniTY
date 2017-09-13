8c56c87d9bfaf407ae7513e0eac78f0adb8fa6c4
Andreas Fernandez <andreas.fernandez@wmdb.de>
2017-09-13 16:34:03 +0200

    [TASK] Module dispatcher with automatic user login
    Resolves: FC-15

---
+++ Resources/Private/Php/module.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+// Exit early if php requirement is not satisfied.
+if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+    die('This version of TYPO3 CMS requires PHP 7.0 or above');
+}
+
+// Set up the application for the backend
+call_user_func(function () {
+    $classLoader = require realpath(__DIR__ . '/../../../../../../typo3/../vendor/autoload.php');
+    require __DIR__ . '/../../../Classes/Http/Application.php';
+
+    (new \WebVision\WvT3unity\Http\Application($classLoader))->run();
+});
