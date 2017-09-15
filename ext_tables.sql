e7f812f7620bda777b87cbc53192c03ef1d958a5
Andreas Fernandez <andreas.fernandez@wmdb.de>
2017-09-15 10:21:52 +0200

    [FEATURE] Single Sign-On
    Resolves: FC-14

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,13 +1,13 @@
 CREATE TABLE pages (
-    unity_path TEXT NOT NULL,
-    canonical_url TEXT NOT NULL
+    unity_path text,
+    canonical_url text
 );
 
 CREATE TABLE pages_language_overlay (
-    unity_path TEXT NOT NULL,
+    unity_path text,
     canonical_url VARCHAR(255) DEFAULT '' NOT NULL
 );
 
 CREATE TABLE be_users (
-	tx_t3unity_standalone tinyint(1) unsigned DEFAULT '1' NOT NULL
+    tx_t3unity_standalone tinyint(1) unsigned DEFAULT '1' NOT NULL
 );
