f5568bd28533e07c34b73062353e9fad4a066191
Ricky Mathew <ricky@web-vision.de>
2020-06-08 16:59:01 +0200

    [TASK] Removing realurl dependencies

--- Classes/Xclass/PageProvider.php
+++ Classes/Xclass/PageProvider.php
@@ -52,7 +52,7 @@ class PageProvider extends \TYPO3\CMS\Backend\ContextMenu\ItemProviders\PageProv
             // Collecting the magento URL saved in TS object
             $magUrl = $tsSetting['lib.']['magurlValue.']['value'];
             $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
-            $urlSegment = $pageRepository->getPage($this->getPreviewPid())['tx_realurl_pathsegment'];
+            $urlSegment = $pageRepository->getPage($this->getPreviewPid())['slug'];
             
             if ($urlSegment != NULL) {
                 // Writing the redirect URL with magento baseURL and with a trailing slash
