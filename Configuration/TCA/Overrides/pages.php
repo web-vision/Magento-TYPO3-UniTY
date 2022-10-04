8ccc584f46a923f6b7b57495eadc21fabf2b4b1d
Mark <m.houben@web-vision.de>
2022-10-04 13:36:37 +0200

    [TASK] Update page tca

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,6 +1,7 @@
 <?php
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 /*
  * This file is part of the wv_t3unity Extension for TYPO3 CMS.
  *
@@ -9,62 +10,46 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
  * The TYPO3 project - inspiring people to share!
  * Copyright (c) 2017 web-vision GmbH
  */
-call_user_func(
-    function ($extKey, $table) {
-        // Localization paths
-        $locallang = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:';
 
-        // Configuration keys
-        $config = 'config';
-        $configExclude = 'exclude';
-        $configLabel = 'label';
-
-        // Configuration values
-        $configTypeInput = 'input';
-
-        ExtensionManagementUtility::addTCAcolumns(
-            $table,
-            [
-                'canonical_url' => [
-                    $configExclude => 1,
-                    $configLabel   => $locallang . 'pages.canonical_url',
-                    $config => [
-                        'type' => $configTypeInput,
-                        'size' => 70,
-                        'max'  => 70,
-                        'eval' => 'trim,nospace,lower',
-                    ],
+(function ($extKey, $table) {
+    ExtensionManagementUtility::addTCAcolumns(
+        $table,
+        [
+            'canonical_url' => [
+                'exclude' => 1,
+                'label'   => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:' . 'pages.canonical_url',
+                'config' => [
+                    'type' => 'input',
+                    'size' => 70,
+                    'max'  => 70,
+                    'eval' => 'trim,nospace,lower',
                 ],
-                'tx_realurl_pathsegment' => [
-                    $configLabel => $locallang . 'pages.tx_realurl_pathsegment',
-                    'displayCond' => 'FIELD:tx_realurl_exclude:!=:1',
-                    $configExclude => 1,
-                    $config => [
-                        'type' => $configTypeInput,
-                        'max' => 255,
-                        'eval' => 'trim,nospace,lower,DmitryDulepov\\Realurl\\Evaluator\\SegmentFieldCleaner',
-                        'readOnly' => 0,
-                    ],
+            ],
+            'tx_realurl_pathsegment' => [
+                'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:' . 'pages.tx_realurl_pathsegment',
+                'displayCond' => 'FIELD:tx_realurl_exclude:!=:1',
+                'exclude' => 1,
+                'config' => [
+                    'type' => 'input',
+                    'max' => 255,
+                    'eval' => 'trim,nospace,lower,DmitryDulepov\\Realurl\\Evaluator\\SegmentFieldCleaner',
+                    'readOnly' => 0,
                 ],
-            ]
-        );
+            ],
+        ]
+    );
 
-        ExtensionManagementUtility::addToAllTCAtypes(
-            $table,
-            'canonical_url',
-            1,
-            'before:keywords'
-        );
+    ExtensionManagementUtility::addToAllTCAtypes(
+        $table,
+        'canonical_url',
+        1,
+        'before:keywords'
+    );
 
-        // Remove tx_realurl_pathoverride from backend
-        $GLOBALS['TCA'][$table]['palettes']['137']['showitem'] = '';
-
-        ExtensionManagementUtility::registerPageTSConfigFile(
-            $extKey,
-            'Configuration/PageTS/Include.ts',
-            'web-vision UniTY - Config'
-        );
-    },
-    'wv_t3unity',
-    'pages'
-);
+    ExtensionManagementUtility::registerPageTSConfigFile(
+        $extKey,
+        'Configuration/PageTS/Include.ts',
+        'web-vision UniTY - Config'
+    );
+}
+)('wv_t3unity', 'pages');
