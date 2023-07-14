c6be6991bbe20c7bd3d320ef479df0b0168cb545
Riad <riad@web-vision.de>
2023-07-14 17:46:51 +0200

    [TASK] Fix ts include and remove deprecated functions

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,30 +12,30 @@ use TYPO3\CMS\Backend\Controller\SimpleDataHandlerController;
     ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wv_t3unity/Configuration/PageTS/Include.tsconfig">'
     );
-
-    ArrayUtility::mergeRecursiveWithOverrule(
-        $GLOBALS['TYPO3_CONF_VARS'],
-        [
-            'SC_OPTIONS' => [
-                't3lib/class.t3lib_tcemain.php' => [
-                    'processDatamapClass' => [
-                        'wv_t3unity' => Tcemain::class,
-                    ],
-                    'processCmdmapClass' => [
-                        'wv_t3unity' => Tcemain::class,
-                    ],
-                ],
-                'tslib/class.tslib_fe.php' => [
-                    'contentPostProc-all' => [
-                        'wv_t3unity' => ContentPostProc::class . '->hookEntry',
-                    ],
-                    'contentPostProc-output' => [
-                        'wv_t3unity' => ContentPostProc::class . '->hookEntry',
-                    ],
-                ],
-            ],
-        ]
-    );
+    // This can probably be remove or needs reimplementation
+    // ArrayUtility::mergeRecursiveWithOverrule(
+    //     $GLOBALS['TYPO3_CONF_VARS'],
+    //     [
+    //         'SC_OPTIONS' => [
+    //             't3lib/class.t3lib_tcemain.php' => [
+    //                 'processDatamapClass' => [
+    //                     'wv_t3unity' => Tcemain::class,
+    //                 ],
+    //                 'processCmdmapClass' => [
+    //                     'wv_t3unity' => Tcemain::class,
+    //                 ],
+    //             ],
+    //             'tslib/class.tslib_fe.php' => [
+    //                 'contentPostProc-all' => [
+    //                     'wv_t3unity' => ContentPostProc::class . '->hookEntry',
+    //                 ],
+    //                 'contentPostProc-output' => [
+    //                     'wv_t3unity' => ContentPostProc::class . '->hookEntry',
+    //                 ],
+    //             ],
+    //         ],
+    //     ]
+    // );
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['mag_cache'] = 'WebVision\\WvT3unity\\Hooks\\ClearMagCache';
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook']['mag_cache'] = PageHook::class . '->render';
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,7 +3,3 @@ CREATE TABLE pages (
     canonical_url TEXT NOT NULL
 );
 
-CREATE TABLE pages_language_overlay (
-    unity_path TEXT NOT NULL,
-    canonical_url VARCHAR(255) DEFAULT '' NOT NULL
-);
