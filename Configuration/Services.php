efcd08698205203645c2fe870f8fa87d21f757f3
Riad <riad@web-vision.de>
2024-07-25 12:05:50 +0200

    [TASK] Only render SEO data and add error logging

---
+++ Configuration/Services.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use TYPO3\CMS\Core\DependencyInjection\SingletonPass;
+use WebVision\WvT3unity\UserFunc\ContentJson;
+
+return function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder) {
+    $containerBuilder
+        ->registerForAutoconfiguration(ContentJson::class)
+        ->addTag('deepl.ContentJson');
+
+    $containerBuilder
+        ->addCompilerPass(new SingletonPass('deepl.ContentJson'));
+};
